2873feb9c022e53897f1da41db297c3e48aeac2d
natsutteatsuiyone <natsutteatsuiyone@gmail.com>
2025-07-16 19:15:49 +0900

    refactor: use clap flatten to deduplicate CLI arguments

--- reversi_cli/src/main.rs
+++ reversi_cli/src/main.rs
@@ -8,11 +8,8 @@ use std::path::{Path, PathBuf};
 use clap::{Parser, Subcommand};
 use reversi_core::types::Selectivity;
 
-#[derive(Parser, Debug)]
-struct Cli {
-    #[command(subcommand)]
-    command: Option<SubCommands>,
-
+#[derive(Parser, Debug, Clone)]
+struct EngineParams {
     #[arg(long, default_value = "64")]
     hash_size: usize,
 
@@ -26,36 +23,27 @@ struct Cli {
     threads: Option<usize>,
 }
 
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<SubCommands>,
+
+    #[command(flatten)]
+    engine_params: EngineParams,
+}
+
 #[derive(Debug, Subcommand)]
 enum SubCommands {
     Gtp {
-        #[arg(long, default_value = "64")]
-        hash_size: usize,
-
-        #[arg(long, default_value = "21")]
-        level: usize,
-
-        #[arg(long, default_value = "1", value_parser = clap::value_parser!(Selectivity).range(1..=6))]
-        selectivity: Selectivity,
-
-        #[arg(long)]
-        threads: Option<usize>,
+        #[command(flatten)]
+        engine_params: EngineParams,
     },
     Solve {
         #[arg()]
         file: PathBuf,
 
-        #[arg(long, default_value = "64")]
-        hash_size: usize,
-
-        #[arg(short, long, default_value = "21")]
-        level: usize,
-
-        #[arg(long, default_value = "1", value_parser = clap::value_parser!(Selectivity).range(1..=6))]
-        selectivity: Selectivity,
-
-        #[arg(long)]
-        threads: Option<usize>,
+        #[command(flatten)]
+        engine_params: EngineParams,
     },
 }
 
@@ -64,28 +52,38 @@ fn main() {
 
     let args = Cli::parse();
     match args.command {
-        Some(SubCommands::Gtp { hash_size, level, selectivity, threads }) => {
-            let mut gtp_engine = gtp::GtpEngine::new(hash_size, level, selectivity, threads);
+        Some(SubCommands::Gtp { engine_params }) => {
+            let mut gtp_engine = gtp::GtpEngine::new(
+                engine_params.hash_size,
+                engine_params.level,
+                engine_params.selectivity,
+                engine_params.threads,
+            );
             gtp_engine.run();
         }
-        Some(SubCommands::Solve {
-            file,
-            hash_size,
-            level,
-            selectivity,
-            threads,
-        }) => {
+        Some(SubCommands::Solve { file, engine_params }) => {
             if !file.exists() {
                 eprintln!("File does not exist: {}", file.display());
                 return;
             }
             let path = Path::new(&file);
-            if let Err(e) = solve::solve(path, hash_size, level, selectivity, threads) {
+            if let Err(e) = solve::solve(
+                path,
+                engine_params.hash_size,
+                engine_params.level,
+                engine_params.selectivity,
+                engine_params.threads,
+            ) {
                 eprintln!("Error solving game: {e}");
             }
         }
         None => {
-            ui::ui_loop(args.hash_size, args.level, args.selectivity, args.threads);
+            ui::ui_loop(
+                args.engine_params.hash_size,
+                args.engine_params.level,
+                args.engine_params.selectivity,
+                args.engine_params.threads,
+            );
         }
     }
 }
