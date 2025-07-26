606e81d66285c27ab29545b2fcf72eb4a80fe571
natsutteatsuiyone <natsutteatsuiyone@gmail.com>
2025-07-26 13:07:49 +0900

    docs: add documentation for types module

--- reversi_core/src/types.rs
+++ reversi_core/src/types.rs
@@ -1,4 +1,13 @@
+//! Common type aliases used throughout the engine.
+
+/// Search depth
 pub type Depth = u32;
+
+/// Score
 pub type Score = i32;
+
+/// Floating-point score
 pub type Scoref = f32;
+
+/// Selectivity level for search pruning
 pub type Selectivity = u8;
